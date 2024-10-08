12c8d93c1e94afae1380a4cd9e121100296f1a6c
HailHydra <darklususnaturae@gmail.com>
2024-10-09 00:42:16 +0530

    Final out

--- init_setup.sh
+++
@@ -1,8 +0,0 @@
-echo [$(date)]: "START"
-echo [$(date)]: "Creating environment with latest python version"
-conda create --prefix ./env python=3.12.4 -y
-echo [$(date)]: "Activating the environment"
-source activate ./env
-echo [$(date)]: "Installing the developer requirements"
-pip install -r requirements_dev.txt
-echo [$(date)]: "END"
--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 from setuptools import find_packages,setup
-from typing import List
-
-"""HYPEN_E_DOT='-e .'
-
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-    return requirements"""
 
 setup(
     name='Gemstone Price Prediction',
-    version='1.0.0',
+    version='1.1.1',
     author='HailHydra',
     author_email='darklususnaturae@gmail.com',
-    install_requires=["scikit-learn","pandas","numpy"],
+    install_requires=[
+        'pandas',
+        'scikit-learn',
+        'numpy',
+        'seaborn',
+        'flask',
+        'mlflow',
+        'ipykernel',
+        'xgboost',
+    ],
     packages=find_packages()
 )
